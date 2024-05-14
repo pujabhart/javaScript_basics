6989d42ae80596996f0ec5c7d80649f31df0b6fb
pujabhart <89658520+pujabhart@users.noreply.github.com>
2024-05-14 10:59:34 +0000

    dataType

---
+++ javaScript_Learn/dataType.js
@@ -0,0 +1,22 @@
+// ##### Primitive
+//  7 Type: String, Number, Boolean, Null, Undefiend, Symbol, BigInt 
+// const score = 100
+// const ScoreValue = 100.05
+// const isLogin = false
+// const Outsidetamp = null
+// let userEmail;
+const id = Symbol('2925')
+const anotherId = Symbol('2925')
+console.log(id == anotherId);
+const bigInteger = 2925393559875n;
+// #### Non Primitive (Reference)
+//  master in javaScript do first things master in Opject and master in Browser webevents 
+//  Array, Object, Function
+ const myArray = [ "Riya", "Nitin", "Rishit", "Rishita", "Kiaan", "Nityam"]
+ const myOnject = {Name: "Riya", age: 14}
+const myFunction = function(){
+    console.log("hello World");
+}
+//  return myFunction;
+console.log(typeof myArray);
+
