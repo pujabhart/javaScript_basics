a71223e7fc02ca42d4545e4265b7492eb6aabf9a
pujabhart <89658520+pujabhart@users.noreply.github.com>
2024-05-14 09:22:02 +0000

    comparisoin

--- javaScript_Learn/comparison.js
+++ javaScript_Learn/comparison.js
@@ -1,16 +1,16 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+console.log("2" > 1);
+console.log("02" > 1);
 
-// console.log(null > 0);
-// console.log(null >= 0);
-// console.log(null < 0);
-// console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null < 0);
+console.log(null == 0);
 
 // === Strick Check
 console.log("3" === 5 );
