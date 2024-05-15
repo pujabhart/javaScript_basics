6703a77b2f8b597aa4927a10c38d0223160c7cba
pujabhart <89658520+pujabhart@users.noreply.github.com>
2024-05-15 13:24:16 +0000

    string

---
+++ javaScript_Learn/07_String.js
@@ -0,0 +1,15 @@
+let name = "Riya Aayushi"
+let marks = 468
+console.log(`My name is ${name} and my total marks is ${marks} `); // sting intrpolation
+
+const gameName = new String('candyCrush')
+console.log(gameName [1]);
+console.log(gameName.__proto__);
+console.log(gameName.length)
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf("y"));
+console.log(gameName.charCodeAt());
+
+const newStringOne = "  PujaBharti  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
