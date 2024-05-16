2573e94363a412c77c95c5a75c50ecfbdb87cc39
pujabhart <89658520+pujabhart@users.noreply.github.com>
2024-05-16 12:27:02 +0000

    nums

---
+++ javaScript_Learn/08_num_math.js
@@ -0,0 +1,23 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const othernumber = 23.987
+console.log(othernumber.toPrecision(2));
+
+const othernumber1 = 123.987
+console.log(othernumber1.toPrecision(2));
+
+const othernumber2 = 123.987
+console.log(othernumber2.toPrecision(5));
+
+const multipalZero = 1000000
+console.log(multipalZero.toLocaleString('en-IN'));
+
+////////////////////// Maths /////////////////////////////
+
