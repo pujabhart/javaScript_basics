cf70689798593761200995cfe57a8163b67cfa68
pujabhart <89658520+pujabhart@users.noreply.github.com>
2024-05-16 12:55:58 +0000

    math and num

--- javaScript_Learn/08_num_math.js
+++ javaScript_Learn/08_num_math.js
@@ -21,3 +21,18 @@ console.log(multipalZero.toLocaleString('en-IN'));
 
 ////////////////////// Maths /////////////////////////////
 
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(2, 4, 20, 0, 1));
+console.log(Math.max(99, 6, 4, 10 ));
+console.log(Math.random()); // Math me randaom ki value hmesha 0 or 1 me.
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+const max = 20
+const min = 10
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
