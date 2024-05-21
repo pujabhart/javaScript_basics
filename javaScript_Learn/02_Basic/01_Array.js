9e4f750f46fd26b266cf21bf77e7c129ba3db3e0
pujabhart <89658520+pujabhart@users.noreply.github.com>
2024-05-21 09:16:30 +0000

    array

---
+++ javaScript_Learn/02_Basic/01_Array.js
@@ -0,0 +1,13 @@
+/// Array 
+const myArray = [1, 2, 3, 4, 5, 6]
+const myHero = ["Naresh Prasad", " Sudhanshu Kumar", "Bachchi Devi"]
+const myArray2 = new Array(1, 2, 3, 4)
+// console.log(myArray[1]);
+// Array Methods
+// myArray.push(2)
+// myArray.push(7)
+// myArray.pop()
+// myArray.shift();
+myArray.unshift(9);
+console.log(myArray);
+
